fba04cb1a287fb24d58f4e2ecbe5773732d8e8c2
muthee-p <pruemu9@gmail.com>
2023-07-09 17:43:05 +0300

    added regitration

---
+++ products/data.js
@@ -0,0 +1,148 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('API server listening on port 3000');
+});
+
+function generateUniqueId() {
+  const uniqueId = Math.floor(Math.random() * 120) + 5;
+
+  return uniqueId;
+}
+
+
+const jsonData = fs.readFileSync('data.json');
+data = JSON.parse(jsonData);
+
+const usersData = fs.readFileSync('../users/users.json');
+let users = JSON.parse(usersData);
+
+//adding a product
+
+app.post('/api/inventory', (req, res) => {
+  
+   const newProduct = req.body;
+
+  const newProductId = generateUniqueId();
+
+
+  const authorId = newProduct.authorId;
+  const author = users.find(user => user.id === authorId);
+
+  if (!author) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+    const product = {
+      id: newProductId,
+      name: newProduct.name,
+      category: newProduct.category,
+      quantity: newProduct.quantity,
+      price: newProduct.price,
+      authorId: author
+    };
+
+  data.push(product);
+  fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+
+  res.json({ message: 'Product added successfully' });
+});
+
+//   let inventory = [];
+  
+//    if (jsonData.length > 0) {
+//     inventory = JSON.parse(jsonData);
+    
+//   }
+
+//   const newProduct = {
+//     id: generateUniqueId(), 
+//     name: req.body.name,
+//     category: req.body.category,
+//     quantity: req.body.quantity,
+//     price: req.body.price
+    
+//   };
+ 
+//   inventory.push(newProduct);
+
+//   // Write the updated Product data back to the JSON file
+//   fs.writeFileSync('data.json', JSON.stringify(inventory, null, 2));
+
+//   res.json({ message: 'Product added successfully', Product: newProduct });
+// });
+
+
+
+//updating a product
+
+
+app.put('/api/inventory/:id', (req, res) => {
+  const productId = req.params.id;
+  const updatedProductData = req.body;
+
+  if (!data || data.length === 0) {
+    return res.status(404).json({ error: 'No products found' });
+  }
+
+  const product = data.find(product => product.id === parseInt(productId));
+
+  if (!product) {
+    return res.status(404).json({ error: 'Product not found' });
+  }
+
+  product.name = updatedProductData.name;
+  product.category = updatedProductData.category;
+  product.quantity = updatedProductData.quantity;
+  product.price = updatedProductData.price;
+
+  fs.writeFileSync('data.json', JSON.stringify(data,  null, 2));
+
+  res.json({ message: 'Product updated successfully' });
+});
+
+
+//delete product
+
+
+
+app.delete('/api/inventory/:id', (req, res) => {
+  const productId = req.params.id;
+
+  if (!data || data.length === 0) {
+    return res.status(404).json({ error: 'No products found' });
+  }
+
+  const productIndex = data.findIndex(product => product.id === parseInt(productId));
+
+  if (productIndex === -1) {
+    return res.status(404).json({ error: 'Product not found' });
+  }
+
+  data.splice(productIndex, 1);
+
+  fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+
+  res.json({ message: 'Product deleted successfully' });
+});
+
+
+//get one product
+
+app.get('/api/inventory/:id', (req, res) => {
+  const productId = req.params.id;
+
+  const product = data.find(product => product.id === parseInt(productId));
+
+  if (!product) {
+    return res.status(404).json({ error: 'Product not found' });
+  }
+
+  res.json(product);
+});
+
+
